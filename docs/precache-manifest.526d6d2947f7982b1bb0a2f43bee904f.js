8641a850d3b887398e3157aeacc7b625aa6ac103
Marian de la Osa <mariandelaosa@gmail.com>
2020-08-10 11:26:04 +0200

    run docs with detail card top

---
+++ docs/precache-manifest.526d6d2947f7982b1bb0a2f43bee904f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "672b8175bb9cc4883a876302b33356e7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8d3379f350b87937d2d0",
+    "url": "./static/css/main.b6378b9e.chunk.css"
+  },
+  {
+    "revision": "c48ef5f0cb3943b33de4",
+    "url": "./static/js/2.61500190.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.61500190.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d3379f350b87937d2d0",
+    "url": "./static/js/main.de9fe072.chunk.js"
+  },
+  {
+    "revision": "bceea23437c8cbccb594",
+    "url": "./static/js/runtime-main.4c2c5298.js"
+  },
+  {
+    "revision": "e83233cdfa4fbd2ca315d4998d98fd2d",
+    "url": "./static/media/RickyMorty.e83233cd.jpg"
+  },
+  {
+    "revision": "c5b9c4389a06e3d17c6e7af0259a62c3",
+    "url": "./static/media/Sanchez.c5b9c438.png"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logo.75509f2e.png"
+  },
+  {
+    "revision": "f31bf933e64672b8857e861d8c597932",
+    "url": "./static/media/morty.f31bf933.png"
+  },
+  {
+    "revision": "74962e684ecbd11448d8ee464e27d38c",
+    "url": "./static/media/nebulosa3.74962e68.jpg"
+  }
+]);
